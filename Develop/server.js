6728c830dd2686b8a6b10eccd4a928568c47968b
duncd <du.derosier@gmail.com>
2022-08-18 20:15:40 -0400

    server.js additions

--- Develop/server.js
+++ Develop/server.js
@@ -20,5 +20,26 @@ app.get('/api/notes', (req,res) => {
     });
 });
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.post('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname,  "./db/db.json"), 'utf8', (err, data) => {
+      let db = JSON.parse(data);
+      db.push({
+        id: uuid.v4(),
+        ...req.body,
+      });
+      fs.writeFile(
+        path.join(__dirname,  "./db/db.json"),
+        JSON.stringify(db, null, 2),
+        (err, data) => {
+          if (err) throw err;
+          res.json(db);
+        }
+      );
+    });
+  });
 
 app.listen(PORT, () => console.log(`The server is now listening on PORT ${PORT}`));
